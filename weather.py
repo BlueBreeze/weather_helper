edcd1c7a8b0788d254ce6a90c8c3477c34629c44
yilu282 <1049924595@qq.com>
2013-03-25 14:15:04 +0800

    modify weather.py

--- weather.py
+++ weather.py
@@ -6,6 +6,7 @@ Created on Mar 22, 2013
 '''
 import urllib2
 import lxml.html as HTML
+import time, json
 
 def get_weather_by_city(city):
     
@@ -25,19 +26,31 @@ def get_weather_by_city(city):
     weather_url = dict_weather[city]
     html = urllib2.urlopen(weather_url).read()
     html = html.decode('gbk', 'ignore')
-    print html
+    #print html
     root = HTML.document_fromstring(html)
     
-    date = root.xpath("//li[@class=\'week-detail-now\']")[0].text_content()
-    PM2d5 = root.xpath("//h3[@id=\'AQIL\']/em")[0].text_content()
-    AQLI = root.xpath("//h3[@id=\'AQIL\']/b")[0].text_content()
+    day = root.xpath("//li[@class=\'week-detail-now\']/b")[0].text_content()
+    night = root.xpath("//li[@class=\'week-detail-now\']/b")[1].text_content()
+    degress = root.xpath("//li[@class=\'week-detail-now\']/i")[0].text_content()
+
+    print day, night, degress
     lifeindex = root.xpath("//ul[@class=\'lifeindex\']/li")
     umbrella = lifeindex[0].text_content()
     clothes = lifeindex[2].text_content()
     air = lifeindex[5].text_content()
-    print date, umbrella, clothes, air
-    print 'PM2.5指数：'+PM2d5, '空气质量：'+AQLI
+    print umbrella, clothes, air
+    
+    #绌烘皵璐ㄩ噺
+    GMT_FORMAT = "new_%a %b %d %Y %H:%M:%S GMT 0800="
+    arg_time = time.strftime(GMT_FORMAT, time.localtime(time.time()))
+    pm25_url = "http://tianqi.2345.com/t/shikuang/"+weather_url.split('/')[-1].split('.')[0]+".js?"+arg_time
+    weather_info = json.loads(urllib2.urlopen(pm25_url).read().split("=")[-1].replace(";",""))['weatherinfo']
+    pm25 = "PM2.5 =" + weather_info['pm25']
+    idx = u"绌烘皵璐ㄩ噺鎸囨暟涓猴細" + weather_info['idx']
+    print pm25, idx
+    weather = [degress, day, night, pm25, idx, air, umbrella, clothes]
+    return " ".join(weather)
  
 if __name__ == "__main__":   
-    city = 'chengdu'
+    city = 'wuhan'
     get_weather_by_city(city)
