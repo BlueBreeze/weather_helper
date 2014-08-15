a9602d1d6258f82b67a03aa3ac83ceacb301f28f
Yan Jiang <j582033@163.com>
2014-08-16 05:03:21 +0800

    urllib替换为requests

--- weather.py
+++ weather.py
@@ -1,64 +1,74 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-Created on Mar 22, 2013
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+import sys
+from threading import Thread
 
-@author: yilulu
-'''
-import urllib2
-import lxml.html as HTML
-import time, json
-
-from city_code import city_code
+city_code = {
+    'beijing': {'code':54511, 'name':u'北京'},
+    'chengdu': {'code':56294, 'name':u'成都'},
+    'hangzhou': {'code':58457, 'name':u'杭州'},
+    'changsha': {'code':57687, 'name':u'长沙'},
+    'wuhan': {'code':57494, 'name':u'武汉'},
+    'fuzhou':{'code':58847, 'name':u'福州'},
+    'ningbo':{'code':58465, 'name':u'宁波'},
+    'shaoyang':{'code':57766,'name':u'邵阳'},
+    'zibo':{'code':54830, 'name':u'淄博'},
+    'linzi':{'code':60815, 'name':u'临淄'}
+}
 
 def get_weather_by_city(city):
-
     base_url = "http://tianqi.2345.com"
     code = city_code[city]['code']
     weather_url = "%s/%s/%s.htm" % (base_url, city, code)
-    #print weather_url
-    html = urllib2.urlopen(weather_url).read()
-    html = html.decode('gbk', 'ignore')
-    #print html
-    root = HTML.document_fromstring(html)
+    root = BeautifulSoup(requests.get(weather_url).content)
 
-    day = root.xpath("//li[@class=\'week-detail-now\']/b")[0].text_content()
-    night = root.xpath("//li[@class=\'week-detail-now\']/b")[1].text_content()
-    degress = root.xpath("//li[@class=\'week-detail-now\']/i")[0].text_content()
+    li_today = root.findAll('li', {'class':'week-detail-now'})[0]
+    day = li_today.findAll('b')[0].text
+    night = li_today.findAll('b')[1].text
 
-    #print day, night, degress
-    lifeindex = root.xpath("//ul[@class=\'lifeindex\']/li")
-    umbrella = lifeindex[0].text_content()
-    clothes = lifeindex[3].text_content()
-    #air = lifeindex[5].text_content()
-    #print umbrella, clothes
+    Temp = li_today.find('i').findAll('font')
+    low = Temp[0].text
+    high = Temp[1].text
+
+    degress = low + "~" + high
+
+    lifeindex = root.findAll('ul', {'class':'lifeindex'})[0].findAll('li')
+    umbrella = lifeindex[4].text
+    clothes = lifeindex[3].text
 
     #空气质量
-    GMT_FORMAT = "new_%a %b %d %Y %H:%M:%S GMT 0800="  
+    GMT_FORMAT = "new_%a %b %d %Y %H:%M:%S GMT 0800="
     arg_time = time.strftime(GMT_FORMAT, time.localtime(time.time()))
     pm25_url = "http://tianqi.2345.com/t/shikuang/%s.js?%s" % (code, arg_time)
-    weather_info = json.loads(urllib2.urlopen(pm25_url).read().split("=")[-1].replace(";",""))['weatherinfo']
-    pm25 = u"PM2.5指数为：" + str(weather_info['pm25'])
-    idx = weather_info['idx']
-    level = ''
-    if idx <= 50:
-        level = u"优"
-    elif idx <= 100:
-        level = u"良"
-    elif idx <= 150:
-        level = u"轻度污染"
-    elif idx <= 200:
-        level = u"中度污染"
-    elif idx <= 300:
-        level = u"重度污染"
+    weather_info = json.loads(requests.get(pm25_url).content.split("=")[-1].replace(";", ""))['weatherinfo']
+    wkeys = weather_info.keys()
+    if 'pm25' in wkeys:
+        pm25 = u"PM2.5指数为：" + str(weather_info['pm25'])
+        level = weather_info['lv_hint']
+        idx = weather_info['idx']
     else:
-        level = u"严重污染"
-    #print pm25, idx
-    idx_info = u"空气质量指数为：%s %s" %(str(idx), level)
+        pm25 = u"PM2.5值无数据"
+        level = u"无"
+        idx = 0
+
+    idx_info = u"空气质量指数为：%s %s" % (str(idx), level)
     date_format = "%b %d %Y"
     date = time.strftime(date_format, time.localtime(time.time()))
     weather = [city_code[city]['name'], date, degress, day, night, pm25, idx_info, umbrella, clothes]
     return "\n".join(weather)
 
-if __name__ == "__main__":
-    city = 'hangzhou'
+def print_weather(city):
     print get_weather_by_city(city)
+    print '--------------------------'
+
+if __name__ == "__main__":
+    if(len(sys.argv) <= 1):
+        print get_weather_by_city('beijing')
+    else:
+        params = sys.argv[1:]
+        for city in params:
+            Thread(target = print_weather, args = [city]).start()
